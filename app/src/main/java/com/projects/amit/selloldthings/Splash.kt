726d25aea0763c243486f766c52f9a767db7b204
kumaramitbca7 <41389692+kumaramitbca7@users.noreply.github.com>
2018-07-22 13:00:33 +0530

    Update Splash.kt

--- app/src/main/java/com/projects/amit/selloldthings/Splash.kt
+++ app/src/main/java/com/projects/amit/selloldthings/Splash.kt
@@ -7,6 +7,6 @@ class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash);
     }
 }
