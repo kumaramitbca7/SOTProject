3d10152a13b4f81936f706be7c004b715d765005
Pooja <poojakshetri020@gmail.com>
2018-07-22 20:23:36 +0530

    b

--- app/src/main/java/com/projects/amit/selloldthings/Splash.kt
+++ app/src/main/java/com/projects/amit/selloldthings/Splash.kt
@@ -2,12 +2,14 @@ package com.projects.amit.selloldthings
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d("fhskjhsjkfhsk","fksdgfksdjhfksdjhfs");
 
     }
 }
