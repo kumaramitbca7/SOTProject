0eaef77e245f04342ad851afe0f1108d6564f30b
Amit Kumar <arytechpro@gmail.com>
2018-07-22 15:09:06 +0530

    New

--- app/src/main/java/com/projects/amit/selloldthings/Splash.kt
+++ app/src/main/java/com/projects/amit/selloldthings/Splash.kt
@@ -2,11 +2,13 @@ package com.projects.amit.selloldthings
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash)
+        Log.d("HAHAHAH","HAHAHAHA")
     }
 }
